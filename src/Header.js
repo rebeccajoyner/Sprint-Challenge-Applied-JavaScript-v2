7e7546a32e77f08fe5f8dcf41ded55b07a66f363
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-07-15 18:44:17 -0400

    header.js add

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+
+        <span className="date">SMARCH 28, 2019</span>
+
+        <h1>Lambda Times</h1>
+
+        <span className="temp">98°</span>
+
+      </div>
+    
+);
+}
+
+
+
+
+export default Header;
